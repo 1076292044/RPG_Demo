f1ef34b33e888dc4b5ce4248a5fdc1359bdbb392
Dark_Master <wwxyzok@live.com>
2019-03-23 18:15:17 +0800

    :)

---
+++ Assets/Scripts/Cam_Mover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam_Mover : MonoBehaviour
+{
+    public float X_mov = 0f;
+    public float Y_mov = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        X_mov = Input.GetAxis("Horizontal");
+        Y_mov = Input.GetAxis("Vertical");
+
+        Debug.Log(X_mov + "X_mov");
+        Debug.Log(Y_mov + "Y_mov");
+
+        Vector2 CamMove_Vec = new Vector2(X_mov, Y_mov);
+
+
+        transform.Translate(CamMove_Vec);
+    }
+}
---
+++ Assets/Scripts/Smooth_Move.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smooth_Move : MonoBehaviour
+{
+    //Bool
+    bool shouldmove = false;
+    bool left, right, up, down = false;
+    bool IsMoving= false;
+    [SerializeField]
+    float speed = 4f;//higher is slower
+    //Vectors
+    public Vector2 goalvecter;
+
+    //inputs
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalvecter = transform.position;//defaults location of goal startpoint
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (shouldmove)
+        {
+            Vector2 position = transform.position;
+
+            transform.position = Vector2.MoveTowards(transform.position, goalvecter, speed * Time.deltaTime);
+            if (position == goalvecter)
+            {
+                goalvecter = position;
+                IsMoving = false;
+            }
+        }
+    }
+    void Update()
+    {
+        Vector2 position = transform.position;//updates position now
+
+        //get the inputs
+        left = Input.GetKey("left");
+        right = Input.GetKey("right");
+        up = Input.GetKey("up");
+        down = Input.GetKey("down");
+
+        if (IsMoving) { return; }
+        //if
+        if (left)
+        {
+            goalvecter = goalvecter + new Vector2(-1, 0);
+            shouldmove = true;
+            IsMoving = true;
+        }
+        if (right)
+        {
+            goalvecter = goalvecter + new Vector2(1, 0);
+            shouldmove = true;
+            IsMoving = true;
+        }
+        if (up)
+        {
+            goalvecter = goalvecter + new Vector2(0, 1);
+            shouldmove = true;
+            IsMoving = true;
+        }
+        if (down)
+        {
+            goalvecter = goalvecter - new Vector2(0, 1);
+            shouldmove = true;
+            IsMoving = true;
+        }
+        if (goalvecter == position)
+        {
+            shouldmove = false;
+            IsMoving = false;
+        }
+    }
+
+
+}
